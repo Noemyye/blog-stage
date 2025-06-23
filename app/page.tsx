501f50334c7f75f6b28f1ee9788ad77bf6185965
noemie <mimisuper48@gmail.com>
2025-06-23 17:28:32 +0200

    start stylling

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="top-0 sticky w-">
-      <p className="f">Blog.</p>
-
-      <Link href={"/entreprise"} />
+    <div className="top-0 sticky w-full ">
+      <Header/>
     </div>
   );
 }
+  
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,19 @@
-
+import Link from "next/link"
 
 export default function Header() {
 
 
     return (
-        <div>
+        <div className="flex justify-content-space-between items-center">
+            <div className="p-4">
+                <h1>Blog.</h1>
+            </div>
             
+            <div>
+                <Link href={"/entreprise"}>Entreprise</Link>
+                <Link href={"/articles"}>Articles</Link>
+                <Link href={"/bilan"}>Bilan</Link>
+            </div>
         </div>
     )
 }
