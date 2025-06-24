ffba8ecfb216e467b39263b47bc93c3e9bf0acf7
noemie <mimisuper48@gmail.com>
2025-06-24 15:46:24 +0200

    page home and redirection

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="top-0 sticky w-full ">
-      <Header/>
+    <div>
+      <Image alt="Bureau" className="w-full h-auto rounded-sm shadow-lg hover:shadow-xl/15 transition-shadow duration-300 ease-in-out" width={1400} height={700} src={"/img/bureau.png"}></Image>
     </div>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,16 +4,30 @@ export default function Header() {
 
 
     return (
-        <div className="flex justify-content-space-between items-center">
-            <div className="p-4">
-                <h1>Blog.</h1>
+        <section className="mt-16 mb-16 flex flex-col items-center lg:mb-12 lg:flex-row lg:justify-between top-0 sticky">
+            <h1 className="text-balance text-6xl font-bold leading-tight tracking-tighter lg:pr-8 lg:text-8xl">
+                Blog<span className="text-rose-200">.</span>
+            </h1>
+            <div className="text-pretty mt-5 text-center text-xl flex gap-20 underline decoration-rose-200 decoration-3 underline-offset-4">
+                <Link
+                    href="/entreprise"
+                    className="transition-transform duration-300 ease-in-out hover:text-rose-200 hover:scale-125"
+                >
+                    Entreprise
+                </Link>
+                <Link
+                    href="/articles"
+                    className="transition-transform duration-300 ease-in-out hover:text-rose-200 hover:scale-125"
+                >
+                    Articles
+                </Link>
+                <Link
+                    href="/bilan"
+                    className="transition-transform duration-300 ease-in-out hover:text-rose-200 hover:scale-125"
+                >
+                    Bilan
+                </Link>
             </div>
-            
-            <div>
-                <Link href={"/entreprise"}>Entreprise</Link>
-                <Link href={"/articles"}>Articles</Link>
-                <Link href={"/bilan"}>Bilan</Link>
-            </div>
-        </div>
+        </section>
     )
 }
